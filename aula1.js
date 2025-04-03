e2827510679516648852812a137342cb3a7ac244
joaquimjk6558 <jantoniofreitasmoreno@gmail.com>
2025-04-03 11:29:49 -0400

    aula 1 - aula java
    aula 1

---
+++ aula1.js
@@ -0,0 +1,31 @@
+var nome = "joao"
+let idade = 25
+const PI = 3.14
+
+let numero = 42 
+let texto = "Isso e uma string"
+let isverdadeiro = true 
+
+
+let n1 = 10 
+let n2 = 5 
+resultado1 = n1 + n2 
+console.log (resultado1)
+
+
+let x1 = 5
+let x2 = 5
+resultado2 = x1 - x2 
+console.log (resultado2)
+
+
+let y1 = 6
+let y2 = 6
+resultado3 = y1/y2
+console.log (resultado3)
+
+
+let z1 = 2
+let z2 = 2
+resultado4 = z1 * z2 
+console.log (resultado4)
